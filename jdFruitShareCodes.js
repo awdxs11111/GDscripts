533ae61d70f9dc18d2f11b23a451f3fd01f784ee
ziyong <61528323+Kyle0816@users.noreply.github.com>
2021-01-17 13:54:01 +0800

    Add files via upload

--- jdFruitShareCodes.js
+++ jdFruitShareCodes.js
@@ -4,15 +4,11 @@
 支持京东N个账号
  */
 //云服务器腾讯云函数等NOde.js用户在此处填写京东东农场的好友码。
-// github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 FruitShareCodes(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let FruitShareCodes = [
-  '2edfa974f3924bbe8dc33704f1b288e9@c2c0fb39b6d4416999606054e09309cd@d573f1a9064c4848b162fdee78b4117a@e96e88792a9a42769214a6698911b15c',
-  'ba5a39e845e14b90b0f97693828294f8@c2c0fb39b6d4416999606054e09309cd@d573f1a9064c4848b162fdee78b4117a@e96e88792a9a42769214a6698911b15c',
-  'ba5a39e845e14b90b0f97693828294f8@2edfa974f3924bbe8dc33704f1b288e9@d573f1a9064c4848b162fdee78b4117a@e96e88792a9a42769214a6698911b15c',
-  'ba5a39e845e14b90b0f97693828294f8@2edfa974f3924bbe8dc33704f1b288e9@c2c0fb39b6d4416999606054e09309cd@e96e88792a9a42769214a6698911b15c',
-  'ba5a39e845e14b90b0f97693828294f8@2edfa974f3924bbe8dc33704f1b288e9@c2c0fb39b6d4416999606054e09309cd@d573f1a9064c4848b162fdee78b4117a',
+  '0a74407df5df4fa99672a037eec61f7e@dbb21614667246fabcfd9685b6f448f3@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@56db8e7bc5874668ba7d5195230d067a',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6@9c52670d52ad4e1a812f894563c746ea@8175509d82504e96828afc8b1bbb9cb3',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东农场互助码
 if (process.env.FRUITSHARECODES) {
--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -4,15 +4,11 @@
 支持京东N个账号
  */
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
-// github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 PLANT_BEAN_SHARECODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PlantBeanShareCodes = [
-  'qaa74hxscwngyw33svzwntil5a@3i6yidunk7vevxhuxhxroedwmtju5pi27hd5dsa@e7lhibzb3zek3clmkhz5evkchy5srqtppa64qra',
-  '7rytnc5iwcpgpfjaom7vslsvdna7fgvvosthuby@3i6yidunk7vevxhuxhxroedwmtju5pi27hd5dsa@e7lhibzb3zek3clmkhz5evkchy5srqtppa64qra',
-  '7rytnc5iwcpgpfjaom7vslsvdna7fgvvosthuby@qaa74hxscwngyw33svzwntil5a@e7lhibzb3zek3clmkhz5evkchy5srqtppa64qra',
-  '7rytnc5iwcpgpfjaom7vslsvdna7fgvvosthuby@qaa74hxscwngyw33svzwntil5a@3i6yidunk7vevxhuxhxroedwmtju5pi27hd5dsa',
-  '7rytnc5iwcpgpfjaom7vslsvdna7fgvvosthuby@3i6yidunk7vevxhuxhxroedwmtju5pi27hd5dsa@e7lhibzb3zek3clmkhz5evkchy5srqtppa64qra',
+  '66j4yt3ebl5ierjljoszp7e4izzbzaqhi5k2unz2afwlyqsgnasq@olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q@e7lhibzb3zek27amgsvywffxx7hxgtzstrk2lba@olmijoxgmjutyx55upqaqxrblt7f3h26dgj2riy',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '4npkonnsy7xi3p6pjfxg6ct5gll42gmvnz7zgoy@6dygkptofggtp6ffhbowku3xgu@mlrdw3aw26j3wgzjipsxgonaoyr2evrdsifsziy@mlrdw3aw26j3wgzjipsxgonaoyr2evrdsifsziy',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有种豆得豆互助码
 if (process.env.PLANT_BEAN_SHARECODES) {
